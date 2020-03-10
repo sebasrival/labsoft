e859202713d7f2d1d6115ef5b9dbb828c6a884fa
Sebastian Rivaldi <sebasrival@gmail.com>
2020-03-10 13:56:53 -0300

    funciona clientes agregar

---
+++ clientes/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Cliente
+
+class ClienteForm(forms.ModelForm):
+    class Meta():
+        model = Cliente
+        fields = '__all__'
+        widgets = {
+            'ruc' : forms.TextInput(attrs={'class': 'form-control'}),
+            'es_entidad': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'cedula' : forms.TextInput(attrs={'class': 'form-control'}),
+            'nombre' : forms.TextInput(attrs={'class': 'form-control'}),
+            'apellido' : forms.TextInput(attrs={'class': 'form-control'}),
+            'razon_social' : forms.TextInput(attrs={'class': 'form-control'}),
+            'descuento' : forms.NumberInput(attrs={'class': 'form-control'}),
+            'bonificacion' : forms.NumberInput(attrs={'class': 'form-control'}),
+            'email' : forms.TextInput(attrs={'class': 'form-control'}),
+            'telefono' : forms.TextInput(attrs={'class': 'form-control'}),
+            'direccion' : forms.TextInput(attrs={'class': 'form-control'}),
+        }
--- clientes/views.py
+++ clientes/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import ClienteForm
 
 # Create your views here.
 
 @login_required()
 def agregar_cliente(request):
-    return render(request, 'agregar_cliente.html')
+    form = ClienteForm
+    if request.method =='POST':
+        print("Imprimiendo POST: ", request.POST)
+        form = ClienteForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'agregar_cliente.html', context)
