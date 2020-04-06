7822341f21a51a52030581fcc808b8c7fb48ba12
Sebastian Rivaldi <sebasrival@gmail.com>
2020-04-06 19:58:12 -0400

    borrando cache

--- clean.sh
+++ clean.sh
@@ -7,16 +7,16 @@ echo -e "Eliminando pycache...\n"
 find . -path "*/__pycache__/*.pyc" -not -name "__init__.py" -delete
 
 # Eliminar base de datos
-echo -e "Eliminando base de datos postgrest labsoft..."
-sudo -u postgres psql -c "DROP DATABASE labsoft;"
+#echo -e "Eliminando base de datos postgrest labsoft..."
+#sudo -u postgres psql -c "DROP DATABASE labsoft;"
 #Creando base de datos
-echo -e "\nCreando base de datos postgrest labsoft..."
-sudo -u postgres psql -c "CREATE DATABASE labsoft;"
+#echo -e "\nCreando base de datos postgrest labsoft..."
+#sudo -u postgres psql -c "CREATE DATABASE labsoft;"
 #Creando Migraciones 
-echo -e "\nCreando migraciones..."
-python manage.py makemigrations
-python manage.py migrate
+#echo -e "\nCreando migraciones..."
+#python manage.py makemigrations
+#python manage.py migrate
 
 #creando user
-echo -e "\nCreando Super usuario..."
-python manage.py createsuperuser
+#echo -e "\nCreando Super usuario..."
+#python manage.py createsuperuser
