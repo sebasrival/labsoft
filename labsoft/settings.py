7db0f6a9bf2d33eb2031600f6d5cd848b1e23a3e
Sebastian <sebasrival@gmail.com>
2020-05-12 16:35:16 -0400

    se agrego el templates editar cliente

--- labsoft/settings.py
+++ labsoft/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'labsoft',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432',
     }
